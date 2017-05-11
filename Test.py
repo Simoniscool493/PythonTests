ab318889bbe0f62fd02444c884d3552ffc703101
Simon O'Neill <supercoolsimon@hotmail.com>
2017-05-11 23:44:12 +0100

    second

--- Test.py
+++ Test.py
@@ -1,6 +1,20 @@
 import random
 import math
 from graphics import *
+
+NumberOfPoints = 50
+NumberOfTreesToGenerate = 40
+NumberOfPointsToMutateForTrainingData = 30
+TreeDepth = 5
+
+DataPointCircleSize = 7
+TreeGridCircleSize = 3
+ScaleFactor = 7
+GridWidth = 50
+GridHeight = 50
+WindowWidth = GridWidth * ScaleFactor
+WindowHeight = GridHeight * ScaleFactor
+
 class dataPoint:
     def __init__(self,X,Y,colorNumber):
         self.x = X
@@ -75,13 +89,14 @@ def calculateCurrentEntrophy(li):
     return total
 
 def drawGrid(win):
-    for i in range(1,30):
-        win.create_line(0,i*10,300,i*10)
-        win.create_line(i*10,0,i*10,300)
+    for i in range(1,GridHeight):
+        win.create_line(0,i*ScaleFactor,WindowWidth,i*ScaleFactor)
+    for i in range(1, GridWidth):
+        win.create_line(i*ScaleFactor,0,i*ScaleFactor,WindowHeight)
 
 def drawPoints(win,points):
     for point in points:
-        c = Circle(Point(point.x * 10, point.y * 10), 7)
+        c = Circle(Point(point.x * ScaleFactor, point.y * ScaleFactor), DataPointCircleSize)
         c.setFill(getColorFromColorNumber(point.colorNumber))
         c.draw(win)
 
@@ -118,20 +133,20 @@ class treeEndNode:
         self.isEnd = True
 
 
-def getColorFor(tree,x,y):
+def getColorNumberFor(tree,x,y):
     if(tree.isEnd):
-        return getColorFromColorNumber(tree.colorToGoTo)
+        return tree.colorToGoTo
     else:
         if(tree.isX):
             if(x<tree.numberToCompare):
-                return getColorFor(tree.leftNode,x,y)
+                return getColorNumberFor(tree.leftNode,x,y)
             else:
-                return getColorFor(tree.rightNode,x,y)
+                return getColorNumberFor(tree.rightNode,x,y)
         else:
             if(y<tree.numberToCompare):
-                return getColorFor(tree.leftNode,x,y)
+                return getColorNumberFor(tree.leftNode,x,y)
             else:
-                return getColorFor(tree.rightNode,x,y)
+                return getColorNumberFor(tree.rightNode,x,y)
 
 def getBestSplits(list):
     bestDifference = 0
@@ -159,7 +174,7 @@ def getBestSplits(list):
     return splitDecision(sides,number,isX)
 
 def buildDecisionTree(depth,list):
-    if(depth>8):
+    if(depth>TreeDepth):
         return treeEndNode(getMostFrequentColor(list))
     else:
         splits = getBestSplits(list)
@@ -168,33 +183,90 @@ def buildDecisionTree(depth,list):
         return treeNode(leftNode,rightNode,splits.number,splits.isX)
 
 def drawTreeGrid(win,tree):
-    for i in range(0, 30):
-        for j in range(0,30):
-            color = getColorFor(tree,i,j)
-            c = Circle(Point((i * 10), (j * 10)), 3)
+    for i in range(0, GridWidth):
+        for j in range(0,GridHeight):
+            color = getColorFromColorNumber(getColorNumberFor(tree,i,j))
+            c = Circle(Point((i * ScaleFactor), (j * ScaleFactor)), TreeGridCircleSize)
             c.setFill(color)
             c.draw(win)
 
+def drawTreeAverageGrid(win,treeAverage):
+    for i in range(0, GridWidth):
+        for j in range(0,GridHeight):
+            color = getColorFromColorNumber(treeAverage[(i,j)])
+            c = Circle(Point((i * ScaleFactor), (j * ScaleFactor)), TreeGridCircleSize)
+            c.setFill(color)
+            c.draw(win)
+
+def getDeepCopy(points):
+    newPoints = list()
+    for point in points:
+        newPoints.append(dataPoint(point.x,point.y,point.colorNumber))
+    return newPoints
+
+def addNoiseToGraph(points):
+    newPoints = getDeepCopy(points)
+    for i in range(0,NumberOfPointsToMutateForTrainingData):
+        pointToChange = random.randint(0, NumberOfPoints-1)
+        if(random.randint(0,1)==1):
+            numberToChangeTo = random.randint(0, GridWidth - 1)
+            newPoints[pointToChange].x = numberToChangeTo
+        else:
+            numberToChangeTo = random.randint(0, GridHeight - 1)
+            newPoints[pointToChange].y = numberToChangeTo
+    return newPoints
+
+def getAverageOfTrees(listOfTrees):
+    retval = dict()
+    for i in range(0,GridWidth):
+        for j in range (0,GridHeight):
+            retval[(i,j)] = getAverageOfTreesForOnePoint(listOfTrees,i,j)
+    return retval
+
+def getAverageOfTreesForOnePoint(listOfTrees,x,y):
+    numReds = 0
+    numGreens = 0
+    numBlues = 0
+    for tree in listOfTrees:
+        colorValue = getColorNumberFor(tree,x,y)
+        if(colorValue==0):
+            numReds+=1
+        elif(colorValue==1):
+            numGreens+=1
+        else:
+            numBlues+=1
+    if((numReds>numGreens) & (numReds>numBlues)):
+        return 0
+    elif(numGreens>numBlues):
+        return 1
+    else:
+        return 2
+
+
+
 #---------------------------------------------
 points = list()
-for i in range(0,30):
-    points.append(dataPoint(random.randint(1,29),random.randint(1,29),random.randint(0,2)))
+for i in range(0,NumberOfPoints):
+    points.append(dataPoint(random.randint(1,GridWidth-1),random.randint(1,GridHeight-1),random.randint(0,2)))
 
-tree = buildDecisionTree(0,points)
-print(tree.isEnd)
+firstTree = buildDecisionTree(0,points)
+win1 = GraphWin("Start", WindowWidth, WindowHeight)
+drawGrid(win1)
+drawTreeGrid(win1,firstTree)
+drawPoints(win1,points)
 
-win = GraphWin("GraphicsBox", 300, 300)
-drawGrid(win)
-drawTreeGrid(win,tree)
-drawPoints(win,points)
+trees = list()
+for i in range(0,NumberOfTreesToGenerate):
+    newPoints = addNoiseToGraph(points)
+    tree = buildDecisionTree(0,newPoints)
+    trees.append(tree)
 
-win.getMouse()
-
-win2 = GraphWin("GraphicsBox2", 300, 300)
+trueTree = getAverageOfTrees(trees)
+win2 = GraphWin("End", WindowWidth, WindowHeight)
+drawGrid(win2)
+drawTreeAverageGrid(win2,trueTree)
 drawPoints(win2,points)
-win2.getMouse()
-win2.close()
-win.close()
 
+win2.getMouse()
 
 
